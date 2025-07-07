41a1290d98a1adb8a1c9facba28336c18b9742ea
ilfcdk <ilfcdk@gmail.com>
2025-07-07 22:26:00 +0300

    2026_07_07_1

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import SwipeCarousel from './carousel/index.js'
+
+const carouselConfig = {
+  containerId: '#carousel',
+  slideId: '.slide',
+  interval: 3000,
+  isPlaying: true
+}
+
+const carousel = new SwipeCarousel(carouselConfig)
+
+carousel.init()
+
+export default carousel
